f49fd72b0ae013a2c5517a98a94d4904793783f5
Ann <ann2001901225@gmail.com>
2022-03-04 16:11:22 +0800

    Scoring

---
+++ src/main/java/Ann/com/Scoring.java
@@ -0,0 +1,10 @@
+package Ann.com;
+
+public class Scoring {
+    public static void main(String[] args) {
+        Student ann = new Student();
+        ann.name="Ann";
+        ann.english = 80;
+        ann.math = 95;
+    }
+}
---
+++ src/main/java/Ann/com/Student.java
@@ -0,0 +1,7 @@
+package Ann.com;
+
+public class Student {
+    String name;
+    int english;
+    int math;
+}
